aab40807d9687b21fe81b0f374975a7ce1b756e4
user <60804817+ichg4354@users.noreply.github.com>
2021-03-06 17:02:07 +0900

    create navigations

--- App.js
+++ App.js
@@ -1,42 +1,9 @@
-import { Asset } from "expo-asset";
-import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import * as Font from "expo-font";
-
-const casheImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-
-const casheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const loadAssets = () => {
-    const images = casheImages([
-      "https://vectorified.com/images/picture-not-available-icon-1.png",
-    ]);
-    const fonts = casheFonts([IonIcons.font]);
-    return Promise.all([...images, ...fonts]);
-  };
-  const onFinish = () => setIsLoading(true);
-  return isLoading ? (
-    <AppLoading
-      startAsync={loadAssets}
-      onFinish={onFinish}
-      onError={console.error}
-    />
-  ) : (
-    <>
-      <Text>HELLO</Text>
-    </>
-  );
+  const [isLogged, setIsLogged] = useState(false);
+  return isLogged ? <Main></Main> : 
 };
 
 export default App;
