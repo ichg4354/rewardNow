3035bc1a81484a5857ae6aac1b28e946c85511ab
user <60804817+ichg4354@users.noreply.github.com>
2021-03-14 21:41:01 +0900

    fix infinite loop created by having onsnapshot without useDefault

--- Components/CollegeContainer.js
+++ Components/CollegeContainer.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { storeService } from "../fBase";
 import CollegeBox from "./CollegeBox";
 
 const CollegeContainer = ({ userId, loggedIn }) => {
   const [colleges, setColleges] = useState([]);
-  storeService
-    .collection("colleges")
-    .orderBy("likes", "desc")
-    .onSnapshot((snap) => {
-      let data = snap.docs.map((each) => ({
-        college: each.data().college,
-        likes: each.data().likes,
-        id: each.id,
-      }));
-      setColleges(data);
-    });
+
+  const getData = () => {
+    storeService
+      .collection("colleges")
+      .orderBy("likes", "desc")
+      .onSnapshot((snap) => {
+        let data = snap.docs.map((each) => ({
+          college: each.data().college,
+          likes: each.data().likes,
+          id: each.id,
+        }));
+        setColleges(data);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return colleges.map((each, key) => (
     <CollegeBox
       key={key}
